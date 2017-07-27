a75b6c91acedbe01f25a630c18bf07359b2b238e
Aaron Goldsmith <aarongoldsmith@msn.com>
2017-07-26 20:58:53 -0700

    add components, env file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar.js';
+
+
 
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+    <SearchBar />
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'))

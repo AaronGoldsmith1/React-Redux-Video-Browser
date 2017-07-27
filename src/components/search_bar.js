11747624eefc40b8291f0672d72929d85a08454d
Aaron Goldsmith <aarongoldsmith@msn.com>
2017-07-26 21:32:37 -0700

    refactor listener

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -3,13 +3,11 @@ import React, { Component } from 'react';
 class SearchBar extends Component {
   render() {
     return(
-      <input onChange={this.onInputChange} />
+      <input onChange={e => console.log(e.target.value)} />
     )
   }
 
-  onInputChange(e) {
-    console.log(e.target.value)
-  }
+
 }
 
 export default SearchBar;

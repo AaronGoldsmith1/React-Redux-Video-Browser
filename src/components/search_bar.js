8f2959bd99c43e6581a52fde69715a6fc8a1bd81
Aaron Goldsmith <aarongoldsmith@msn.com>
2017-07-26 22:01:49 -0700

    update input control field

--- src/components/search_bar.js
+++ src/components/search_bar.js
@@ -1,10 +1,21 @@
 import React, { Component } from 'react';
 
 class SearchBar extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {term: ''}
+  }
+
   render() {
     return(
-      <input onChange={e => console.log(e.target.value)} />
-    )
+      <div>
+      <input
+      value={this.state.term}
+      onChange={e => this.setState({term: e.target.value})} />
+
+      </div>
+    );
   }
 
 

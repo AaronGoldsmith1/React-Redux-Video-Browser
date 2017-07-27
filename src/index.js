997416b67239580329ef0723c123b96cef6bbfd9
Aaron Goldsmith <aarongoldsmith@msn.com>
2017-07-26 22:30:34 -0700

    update api key storage

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import YT_API from '../api_key.js';
+
 
 import SearchBar from './components/search_bar.js';
 import VideoList from './components/video_list.js';
 
+YTSearch({key: YT_API, term: 'surfboards'}, function(data){
+  console.log(data)
+})
 
 
 const App = () => {
--- webpack.config.js
+++ webpack.config.js
@@ -5,12 +5,6 @@ module.exports = {
   entry: [
     './src/index.js'
   ],
-  plugins: [
-   new Dotenv({
-     path: './.env', // Path to .env file (this is the default)
-     safe: true // load .env.example (defaults to "false" which does not use dotenv-safe)
-   })
- ],
   output: {
     path: __dirname,
     publicPath: '/',

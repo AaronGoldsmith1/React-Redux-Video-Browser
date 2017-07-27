fcc26350fd692b5fd0426158c34fdcdec4409bcc
Aaron Goldsmith <aarongoldsmith@msn.com>
2017-07-26 22:54:40 -0700

    making get request to youtube api

--- src/index.js
+++ src/index.js
@@ -1,23 +1,36 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
+
 import YT_API from '../api_key.js';
-
-
 import SearchBar from './components/search_bar.js';
 import VideoList from './components/video_list.js';
 
-YTSearch({key: YT_API, term: 'surfboards'}, function(data){
-  console.log(data)
-})
 
 
-const App = () => {
-  return (
-    <div>
-    <SearchBar />
-    </div>
-  );
+
+
+class App extends Component {
+  constructor(props){
+    super(props)
+
+    this.state = {videos: []};
+
+    YTSearch({key: YT_API, term: 'surfboards'}, (videos) => {
+      this.setState({videos})
+    });
+
+
+  }
+  render() {
+    return (
+      <div>
+      <SearchBar />
+      </div>
+    )
+
+
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'))

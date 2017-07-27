43deff8f897eb9aeb760d5e8f518bd10e9766100
Aaron Goldsmith <aarongoldsmith@msn.com>
2017-07-26 20:55:41 -0700

    add components, env file

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import SearchBar from './components/search_bar.js';
+
+//TODO: .env file
+const API_KEY = 'AIzaSyDYuGLrcwYCMKfxqv1AL_0LQ46XQq5aePI'
 
 const App = () => {
-  return <div>Hi!</div>;
+  return (
+    <div>
+    <SearchBar />
+    </div>
+  );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'))
